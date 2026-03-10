5b9f2c7b065b7f547baf21f40ebb3492f88c1fc7
alef <alefamaraltads@hotmail.com>
2026-03-10 14:01:15 -0300

    feat: commit inicial do projeto Spring Boot

---
+++ src/main/java/com/barbearia/agendacdb/AgendaCdBApplication.java
@@ -0,0 +1,13 @@
+package com.barbearia.agendacdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaCdBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaCdBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbearia/agendacdb/AgendaCdBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbearia.agendacdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaCdBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
